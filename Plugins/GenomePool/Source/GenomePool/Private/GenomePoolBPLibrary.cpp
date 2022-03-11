ac6af57dc5832d7d3b0d3e2b6e9a3e8a7868cc18
Jeff Kesselman <jpkessel@purdue.edu>
2022-03-11 11:49:54 -0500

    inital plugin commit

---
+++ Plugins/GenomePool/Source/GenomePool/Private/GenomePoolBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GenomePoolBPLibrary.h"
+#include "GenomePool.h"
+
+UGenomePoolBPLibrary::UGenomePoolBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UGenomePoolBPLibrary::GenomePoolSampleFunction(float Param)
+{
+	return -1;
+}
+
